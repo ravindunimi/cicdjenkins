3a46664d92e0a9c671f844929e6d199b33a56ae0
ravindunimi <ravindu@nimidev.com>
2023-06-21 20:17:29 +0530

    initial code

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["Francho", "Layla", "Peter", "Kevin"] });
+});
+
+app.get("/members", (req, res) => {
+  res.json({ users: ["Ravind", "David", "Victor"] });
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
