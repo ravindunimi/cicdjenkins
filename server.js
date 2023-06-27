db9d560dbc1cdd907284476e88e5d950a75c4d39
ravindunimi <118248445+ravindunimi@users.noreply.github.com>
2023-06-27 10:04:46 +0530

    change port

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/members", (req, res) => {
   res.json({ users: ["Ravind", "David", "Victor"] });
 });
 
-app.listen(5001, () => {
+app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
